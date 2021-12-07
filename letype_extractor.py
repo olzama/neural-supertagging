a79e8887684de9177cdcf93a38103d3667061653
Olga Zamaraeva <olzama@uw.edu>
2021-12-07 17:17:55 +0100

    Go over all files, log exceptions

--- letype_extractor.py
+++ letype_extractor.py
@@ -1,5 +1,5 @@
 from delphin import tdl, itsdb
-
+import glob, os
 
 
 
@@ -12,23 +12,38 @@ def parse_lexicons(lexicon_files):
     return lextypes
 
 def process_testsuites(testsuites,lextypes):
-    for testsuite in testsuites:
-        ts = itsdb.TestSuite(testsuite)
-        pairs = []
-        for response in ts.processed_items():
-            result = response.result(0)
-            deriv = result.derivation()
-            for t in deriv.terminals():
-                pairs.append((t.form, t._parent.entity))
-            break
-        with open('./output/'+ts.path.stem+'.txt', 'w') as f:
-            for form, entity in pairs:
-                letype = lextypes.get(entity, None)
-                str_pair = f'{form}\t{letype}'
-                print(str_pair)
-                f.write(str_pair+'\n')
+    log = ''
+    with open('./errors.txt', 'w') as logf:
+        for testsuite in glob.iglob(testsuites+'**'):
+            try:
+                ts = itsdb.TestSuite(testsuite)
+                print("Processing " + ts.path.stem)
+            except:
+                print("ERROR: " + ts.path.stem)
+                logf.write("TESTSUITE ERROR: " + ts.path.stem + '\n')
+            pairs = []
+            items = list(ts.processed_items())
+            for i,response in enumerate(items):
+                if len(response['results']) > 0:
+                    if i%100==0:
+                        print("Processing item {} out of {}...".format(i,len(items)))
+                    result = response.result(0)
+                    deriv = result.derivation()
+                    for t in deriv.terminals():
+                        pairs.append((t.form, t._parent.entity))
+                else:
+                    err = response['error'] if response['error'] else 'None'
+                    log += ts.path.stem + '\t' + str(response['i-id']) + '\t' + response['i-input'] + '\t' + err +'\n'
+                #break
+            with open('./output/'+ts.path.stem+'.txt', 'w') as f:
+                for form, entity in pairs:
+                    letype = lextypes.get(entity, None)
+                    str_pair = f'{form}\t{letype}'
+                    f.write(str_pair+'\n')
+            logf.write(log)
+
 
 if __name__ == "__main__":
     lextypes = parse_lexicons(['/Users/olzama/Research/ERG/2020/lexicon.tdl','/Users/olzama/Research/ERG/2020/ple.tdl',
                                '/Users/olzama/Research/ERG/2020/gle.tdl','/Users/olzama/Research/ERG/2020/lexicon-rbst.tdl'])
-    process_testsuites(['/Users/olzama/Research/ERG/2020/tsdb/gold/bcs'], lextypes)
+    process_testsuites('/Users/olzama/Research/ERG/2020/tsdb/tag/', lextypes)
