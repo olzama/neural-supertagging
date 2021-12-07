451d4fd511bfa79ce1eef23f6672030153f2679d
Olga Zamaraeva <olzama@uw.edu>
2021-12-07 15:40:21 +0100

    refactored basic example.

--- letype_extractor.py
+++ letype_extractor.py
@@ -1,14 +1,27 @@
 from delphin import tdl, itsdb
-lextypes = {}  # mapping of lexical entry IDs to types
-for event, obj, lineno in tdl.iterparse('/Users/olzama/Research/ERG/trunk/lexicon.tdl'):
-    if event == 'TypeDefinition':
-        lextypes[obj.identifier] = obj.supertypes[0]  # assume exactly 1
 
-ts = itsdb.TestSuite('/Users/olzama/Research/ERG/trunk/tsdb/gold/mrs')
-for response in ts.processed_items():
-    result = response.result(0)
-    deriv = result.derivation()
-    pairs = [(t.form, t._parent.entity) for t in deriv.terminals()]
-    print(' '.join(f'{form}/{lextypes.get(entity, entity)}'
-                   for form, entity in pairs))
-    break  # for one example only
+
+
+
+def parse_lexicons(lexicon_files):
+    lextypes = {}  # mapping of lexical entry IDs to types
+    for lexicon in lexicon_files:
+        for event, obj, lineno in tdl.iterparse(lexicon):
+            if event == 'TypeDefinition':
+                lextypes[obj.identifier] = obj.supertypes[0]  # assume exactly 1
+    return lextypes
+
+def process_testsuite(testsuite_path,lextypes):
+    ts = itsdb.TestSuite(testsuite_path)
+    for response in ts.processed_items():
+        result = response.result(0)
+        deriv = result.derivation()
+        pairs = [(t.form, t._parent.entity) for t in deriv.terminals()]
+        print(' '.join(f'{form}/{lextypes.get(entity, entity)}'
+                       for form, entity in pairs))
+        break  # for one example only
+
+
+if __name__ == "__main__":
+    lextypes = parse_lexicons(['/Users/olzama/Research/ERG/trunk/lexicon.tdl'])
+    process_testsuite('/Users/olzama/Research/ERG/trunk/tsdb/gold/mrs', lextypes)
