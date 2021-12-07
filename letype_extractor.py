d146659ece8a9b9848a03619cc016b26f688d8ce
Olga Zamaraeva <olzama@uw.edu>
2021-12-07 16:23:30 +0100

    sample output

--- letype_extractor.py
+++ letype_extractor.py
@@ -11,17 +11,24 @@ def parse_lexicons(lexicon_files):
                 lextypes[obj.identifier] = obj.supertypes[0]  # assume exactly 1
     return lextypes
 
-def process_testsuite(testsuite_path,lextypes):
-    ts = itsdb.TestSuite(testsuite_path)
-    for response in ts.processed_items():
-        result = response.result(0)
-        deriv = result.derivation()
-        pairs = [(t.form, t._parent.entity) for t in deriv.terminals()]
-        print(' '.join(f'{form}/{lextypes.get(entity, entity)}'
-                       for form, entity in pairs))
-        break  # for one example only
-
+def process_testsuites(testsuites,lextypes):
+    for testsuite in testsuites:
+        ts = itsdb.TestSuite(testsuite)
+        pairs = []
+        for response in ts.processed_items():
+            result = response.result(0)
+            deriv = result.derivation()
+            for t in deriv.terminals():
+                pairs.append((t.form, t._parent.entity))
+            break
+        with open('./output/'+ts.path.stem+'.txt', 'w') as f:
+            for form, entity in pairs:
+                letype = lextypes.get(entity, None)
+                str_pair = f'{form}\t{letype}'
+                print(str_pair)
+                f.write(str_pair+'\n')
 
 if __name__ == "__main__":
-    lextypes = parse_lexicons(['/Users/olzama/Research/ERG/trunk/lexicon.tdl'])
-    process_testsuite('/Users/olzama/Research/ERG/trunk/tsdb/gold/mrs', lextypes)
+    lextypes = parse_lexicons(['/Users/olzama/Research/ERG/2020/lexicon.tdl','/Users/olzama/Research/ERG/2020/ple.tdl',
+                               '/Users/olzama/Research/ERG/2020/gle.tdl','/Users/olzama/Research/ERG/2020/lexicon-rbst.tdl'])
+    process_testsuites(['/Users/olzama/Research/ERG/2020/tsdb/gold/bcs'], lextypes)
