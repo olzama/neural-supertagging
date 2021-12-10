fb47caea70e2c2a2f05809fef03367fd0ed66142
Olga Zamaraeva <olzama@uw.edu>
2021-12-10 17:10:50 +0100

    Fixed overcounting bug due to counting the previous corpus twice if the next corpus failed to load. Fixed reporting wrong failed corpora names.

--- letype_extractor.py
+++ letype_extractor.py
@@ -19,37 +19,38 @@ class LexTypeExtractor:
             for j,testsuite in enumerate(glob.iglob(testsuites+'**')):
                 try:
                     ts = itsdb.TestSuite(testsuite)
+                    print("Processing " + ts.path.stem)
+                    self.stats['corpora'].append({'name': ts.path.stem})
+                    pairs = []
+                    items = list(ts.processed_items())
+                    self.stats['corpora'][j]['items'] = len(items)
+                    self.stats['corpora'][j]['noparse'] = 0
+                    for i, response in enumerate(items):
+                        if len(response['results']) > 0:
+                            if i % 100 == 0:
+                                print("Processing item {} out of {}...".format(i, len(items)))
+                            result = response.result(0)
+                            deriv = result.derivation()
+                            for t in deriv.terminals():
+                                if not t.form in self.stats['tokens']:
+                                    self.stats['tokens'][t.form] = 0
+                                self.stats['tokens'][t.form] += 1
+                                pairs.append((t.form, t.parent.entity))
+                        else:
+                            self.stats['corpora'][j]['noparse'] += 1
+                            err = response['error'] if response['error'] else 'None'
+                            logf.write(ts.path.stem + '\t' + str(response['i-id']) + '\t'
+                                       + response['i-input'] + '\t' + err + '\n')
+                    with open('./output/' + ts.path.stem + '.txt', 'w') as f:
+                        for form, entity in pairs:
+                            letype = lextypes.get(entity, None)
+                            str_pair = f'{form}\t{letype}'
+                            f.write(str_pair + '\n')
                 except:
-                    print("ERROR: " + ts.path.stem)
-                    self.stats['failed corpora'].append({'name':ts.path.stem})
-                    logf.write("TESTSUITE ERROR: " + ts.path.stem + '\n')
-                print("Processing " + ts.path.stem)
-                self.stats['corpora'].append({'name':ts.path.stem})
-                pairs = []
-                items = list(ts.processed_items())
-                self.stats['corpora'][j]['items'] = len(items)
-                self.stats['corpora'][j]['noparse'] = 0
-                for i,response in enumerate(items):
-                    if len(response['results']) > 0:
-                        if i%100==0:
-                            print("Processing item {} out of {}...".format(i,len(items)))
-                        result = response.result(0)
-                        deriv = result.derivation()
-                        for t in deriv.terminals():
-                            if not t.form in self.stats['tokens']:
-                                self.stats['tokens'][t.form] = 0
-                            self.stats['tokens'][t.form] += 1
-                            pairs.append((t.form, t.parent.entity))
-                    else:
-                        self.stats['corpora'][j]['noparse'] += 1
-                        err = response['error'] if response['error'] else 'None'
-                        logf.write(ts.path.stem + '\t' + str(response['i-id']) + '\t'
-                                   + response['i-input'] + '\t' + err +'\n')
-                with open('./output/'+ts.path.stem+'.txt', 'w') as f:
-                    for form, entity in pairs:
-                        letype = lextypes.get(entity, None)
-                        str_pair = f'{form}\t{letype}'
-                        f.write(str_pair+'\n')
+                    print("ERROR: " + testsuite)
+                    self.stats['failed corpora'].append({'name':testsuite})
+                    self.stats['corpora'].append(None)
+                    logf.write("TESTSUITE ERROR: " + testsuite + '\n')
 
 
 
@@ -61,8 +62,9 @@ if __name__ == "__main__":
     le.process_testsuites(args[1],le.lextypes)
     with open('stats.txt','w') as f:
         for c in le.stats['corpora']:
-            for item in c:
-                f.write(str(item) + ': ' + str(c[item]) + '\n')
+            if c:
+                for item in c:
+                    f.write(str(item) + ': ' + str(c[item]) + '\n')
         f.write('Failed to load corpora:' + str(len(le.stats['failed corpora'])) + '\n')
         for fc in le.stats['failed corpora']:
             f.write(fc['name'] + '\n')
