effff037076604f01d8000d5b41ba06fd59919ea
Olga Zamaraeva <olzama@uw.edu>
2022-01-24 13:20:48 +0100

    Load serialized training vectors to train

--- classic-classifiers.py
+++ classic-classifiers.py
@@ -15,8 +15,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.exceptions import ConvergenceWarning
 from sklearn import svm
 
-from vectorizer import read_data,vectorize_data
-
 warnings.filterwarnings("ignore", category=ConvergenceWarning, module="sklearn")
 
 
@@ -29,6 +27,7 @@ def train_SVM(X, Y):
 def train_MaxEnt(X, Y):
     solver = "saga" # Another option is "sag"; it was also tried in development
     train_samples, n_features = X.shape
+    n_classes = np.unique(Y).shape[0]
     print(
         "Dataset ERG treebanks, train_samples=%i, n_features=%i, n_classes=%i"
         % (train_samples, n_features, n_classes)
@@ -46,7 +45,7 @@ def train_MaxEnt(X, Y):
     # The overall best MaxEnt model (high accuracy, low train time, best test time, out of other MaxEnts
     # This assumes SAGA solver; with SAG OVR L1, can get higher accuracy but training time is huge.
     models = {
-        'l2': {"multinomial": {"name": "Multinomial-L2", "iters": [100]}}
+        'l2': {"multinomial": {"name": "Multinomial-L2", "iters": [1]}}
     }
 
     for penalty in models:
@@ -78,9 +77,8 @@ def fit_serialize(X, Y, clf, name):
         pickle.dump(clf, f)
 
 
-def test_model(model, X_test, Y_test, n_classes, corpus_id,num_sentences,by_sentence):
-    with open(model, 'rb') as f:
-        clf = pickle.load(f)
+
+def test_model(clf, X_test, Y_test, num_sentences):
     y_pred = []
     Y_gold = []
     times = []
@@ -91,63 +89,34 @@ def test_model(model, X_test, Y_test, n_classes, corpus_id,num_sentences,by_sent
         times.append(test_time)
         y_pred = y_pred + list(pred)
     t_sum = sum(times)
-    if by_sentence:
-        avg_time = t_sum/len(X_test)
-    else:
-        avg_time = t_sum/num_sentences
+    avg_time = t_sum/num_sentences
     print('Test time of {}: {}; {} average per sentence'.format(model, t_sum, avg_time))
     for sentence_labels in Y_test:
         Y_gold = Y_gold + list(sentence_labels)
     accuracy = np.sum(np.array(y_pred) == np.array(Y_gold)) / len(Y_gold)
-    #density = np.mean(clf.coef_ != 0, axis=1) * 100
-    print("Test accuracy for model %s: %.4f on corpus %s" % (model, accuracy, str(corpus_id)))
+    print("Test accuracy for model %s: %.4f" % (model, accuracy))
     return accuracy
-    # print(
-    #     "%% non-zero coefficients for model %s, per class:\n %s"
-    #     % (model, densities[-1])
-    # )
 
-
+def load_vectors(path_to_vecs, path_to_labels):
+    with open(path_to_vecs, 'rb') as vf:
+        vecs = pickle.load(vf)
+    with open(path_to_labels,'rb') as lf:
+        labels = pickle.load(lf)
+    return vecs, labels
 
 if __name__ == "__main__":
-    feature_dicts, true_labels, n_train, test_sen_lengths, test_corpus_lengths\
-        = read_data('./data/contexts/', './data/true_labels/')
-    X_train, X_test, Y = vectorize_data(feature_dicts,true_labels,n_train)
-    n_classes = np.unique(Y).shape[0]
-    Y_train = Y[:n_train]
-    Y_test = Y[n_train:]
     if sys.argv[1] == 'train':
+        X, Y = load_vectors(sys.argv[2], sys.argv[3])
         #train_SVM(X_train,Y_train)
-        train_MaxEnt(X_train, Y_train)
+        train_MaxEnt(X, Y)
     elif sys.argv[1] == 'test':
-        test_corpora = []
-        true_labels_per_corpus = []
-        start = 0
-        corpus_start = 0
-        # TODO: do this better, via parse_args()
-        by_sentence = sys.argv[2] == 'True'
-        for corpus in test_corpus_lengths:
-            if by_sentence:
-                X_test_per_sentence = []
-                Y_test_per_sentence = []
-                test_sentences = test_sen_lengths[corpus_start:corpus_start + test_corpus_lengths[corpus]]
-                for l in test_sentences:
-                    X_test_per_sentence.append(X_test[start:start + l])
-                    Y_test_per_sentence.append(Y_test[start:start + l])
-                    start = start + l
-                test_corpora.append(X_test_per_sentence)
-                true_labels_per_corpus.append(Y_test_per_sentence)
-            else:
-                test_corpora.append([X_test]) #TODO this is a bug; adding all the corpora many times
-                true_labels_per_corpus.append([Y_test])
-            corpus_start = corpus_start + test_corpus_lengths[corpus]
-        corpus_names = list(test_corpus_lengths.keys())
+        with open(sys.argv[2], 'rb') as cf:
+            corpus = pickle.load(cf)
         model_accuracies = []
         for model in glob.iglob('models/' + '*'):
+            with open(model, 'rb') as f:
+                clf = pickle.load(f)
             accuracies = []
-            for i,tc in enumerate(test_corpora):
-                corpus_name = corpus_names[i]
-                acc = test_model(model,tc,true_labels_per_corpus[i],n_classes,corpus_name,
-                                 test_corpus_lengths[corpus_name],by_sentence)
-                accuracies.append(acc)
+            acc = test_model(clf,corpus.X,corpus.Y,len(corpus.sen_lengths))
+            accuracies.append(acc)
             model_accuracies.append(accuracies)
