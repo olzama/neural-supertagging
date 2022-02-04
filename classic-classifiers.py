75ad75d17cb07fc54c5685f2bea417eb1502896c
Olga Zamaraeva <olzama@uw.edu>
2022-02-04 11:27:40 +0100

    svm setup

--- classic-classifiers.py
+++ classic-classifiers.py
@@ -97,8 +97,8 @@ def load_vectors(path_to_vecs, path_to_labels):
 if __name__ == "__main__":
     if sys.argv[1] == 'train':
         X, Y = load_vectors(sys.argv[2], sys.argv[3])
-        #train_SVM(X,Y)
-        train_MaxEnt(X, Y, all=True)
+        train_SVM(X,Y)
+        #train_MaxEnt(X, Y, all=True)
     elif sys.argv[1] == 'test':
         corpora = []
         if os.path.isdir(sys.argv[2]):
