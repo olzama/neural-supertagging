6a2d7b78d006a6f6812924e46983fef92db9a1cb
Olga Zamaraeva <olzama@uw.edu>
2022-03-07 18:02:54 +0100

    work in progress for autoregressive training

--- classic-classifiers.py
+++ classic-classifiers.py
@@ -15,8 +15,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.exceptions import ConvergenceWarning
 from sklearn import svm
 
-warnings.filterwarnings("ignore", category=ConvergenceWarning, module="sklearn")
 
+warnings.filterwarnings("ignore", category=ConvergenceWarning, module="sklearn")
 
 def train_SVM(X, Y):
     clf = svm.LinearSVC() # Kernels would be too slow, so using liblinear SVM
@@ -24,14 +24,14 @@ def train_SVM(X, Y):
     fit_serialize(X,Y,clf,name) # for models over 4GB, need to add protocol=4
 
 
-def train_MaxEnt(X, Y, all=False):
+def train_MaxEnt(X, Y, all=False, autoreg=False,vec=None,lbl_enc=None):
     solver = "saga" # Another option is "sag"; it was also tried in development
-    train_samples, n_features = X.shape
-    n_classes = np.unique(Y).shape[0]
-    print(
-        "Dataset ERG treebanks, train_samples=%i, n_features=%i, n_classes=%i"
-        % (train_samples, n_features, n_classes)
-    )
+    # train_samples, n_features = X.shape
+    # n_classes = np.unique(Y).shape[0]
+    # print(
+    #     "Dataset ERG treebanks, train_samples=%i, n_features=%i, n_classes=%i"
+    #     % (train_samples, n_features, n_classes)
+    # )
 
     if all:
         # All MaxEnt models tried in development:
@@ -64,8 +64,10 @@ def train_MaxEnt(X, Y, all=False):
                     l1_ratio=0.5 # only for elastic-net
                 )
                 model_name = models[penalty][model]["name"] + '-' + solver
-                fit_serialize(X, Y, clf, model_name)
-
+                if not autoreg:
+                    fit_serialize(X, Y, clf, model_name)
+                else:
+                    fit_serialize_autoreg(X,Y,clf,model_name,vec,lbl_enc)
 
 def fit_serialize(X, Y, clf, name):
     t1 = timeit.default_timer()
@@ -75,6 +77,25 @@ def fit_serialize(X, Y, clf, name):
     with open('models/' + name + '.model', 'wb') as f:
         pickle.dump(clf, f)
 
+def fit_serialize_autoreg(X, Y, clf, name,vec,lbl_enc):
+    t1 = timeit.default_timer()
+    for i,tok_row in enumerate(X):
+        clf.fit(tok_row, Y[i])
+        y_train_i = clf.predict(tok_row)
+        train_acc_i = np.sum(np.array(Y[i]) == np.array(y_train_i)) / len(Y[i])
+        print('Processed row {}; accuracy {}'.format(i,train_acc_i))
+        update_row(tok_row, y_train_i,vec,lbl_enc)
+    train_time = timeit.default_timer() - t1
+    print('Training time of {}: {}'.format(name, train_time))
+    with open('models/' + name + '.model', 'wb') as f:
+        pickle.dump(clf, f)
+
+def update_row(X_i,y_i,vec,lbl_enc):
+    for obs in X_i:
+        print(vec.inverse_transform(obs))
+        y_pred = lbl_enc[y_i]
+        for f in obs:
+            print(f)
 
 
 def test_model(clf, X_test, Y_test, num_sentences):
@@ -95,10 +116,17 @@ def load_vectors(path_to_vecs, path_to_labels):
     return vecs, labels
 
 if __name__ == "__main__":
+    with open('./output/vectorized/vectorizer', 'rb') as vf:
+        vec = pickle.load(vf)
+    with open('./output/vectorized/label-dict', 'rb') as lf:
+        lbl_enc = pickle.load(lf)
+    with open('./output/vectorized/label-inv-dict', 'rb') as lf:
+        lbl_inv_enc = pickle.load(lf)
     if sys.argv[1] == 'train':
+        autoregressive = sys.argv[4] == 'autoreg'
         X, Y = load_vectors(sys.argv[2], sys.argv[3])
-        train_SVM(X,Y)
-        train_MaxEnt(X, Y, all=True)
+        #train_SVM(X,Y)
+        train_MaxEnt(X, Y, all=False, autoreg=True,vec=vec,lbl_enc=lbl_inv_enc)
     elif sys.argv[1] == 'test':
         corpora = []
         if os.path.isdir(sys.argv[2]):
