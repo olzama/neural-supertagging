8b414c47e7ce88b47ee2271568bd05bb666bd048
Olga Zamaraeva <olzama@uw.edu>
2023-01-03 14:00:55 +0100

    learning rate 1e-5

--- huggingface/create_hf_dataset.py
+++ huggingface/create_hf_dataset.py
@@ -252,9 +252,9 @@ if __name__ == '__main__':
     #features = dataset['train'].features['labels']
 
     training_args = TrainingArguments(
-        output_dir="/media/olga/kesha/BERT/erg/trainer-3e-5/",
+        output_dir="/media/olga/kesha/BERT/erg/trainer-1e-5/",
         evaluation_strategy = "epoch",
-        learning_rate=3e-5,
+        learning_rate=1e-5,
         num_train_epochs= 50,
         weight_decay=0.01,
         save_strategy = "no"
