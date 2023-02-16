dd11e99e178df5721b3d2ecba6ee28dc899ee907
olzama <olga.zamaraeva@gmail.com>
2023-02-16 18:06:00 +0100

    test maintenance

--- Test.py
+++ Test.py
@@ -1,6 +1,9 @@
 import unittest
+import pickle
 from tsdb.tok_classification import Token_Tag_Extractor
 from huggingface import create_hf_dataset
+from classic_supertagging import vectorizer
+
 
 LEXICONS = 'tsdb/test-treebanks/lexicons/'
 TREEBANKS = 'tsdb/test-treebanks/treebanks-small/'
@@ -18,8 +21,8 @@ class ExampleTestCase(unittest.TestCase):
 
 # Check that the Lexical Type Extractor is counting things correctly,
 # using the well-known MRS testsuite.
-class MRSTest(unittest.TestCase):
-    def test_tte_processing(self):
+class TestTTEProcessing(unittest.TestCase):
+    def test(self):
         tte = Token_Tag_Extractor()  # This extracts token-tag pairs, per corpus, sentences separated by special character
         lextypes = tte.parse_lexicons(LEXICONS)
         data = tte.process_testsuites(TREEBANKS, lextypes)
@@ -28,7 +31,7 @@ class MRSTest(unittest.TestCase):
         self.assertEqual(len(data['train'][0].processed_data), 107)  # should contain 107 token-tag pairs
 
 class TestCreateHFDataset(unittest.TestCase):
-    def test_create_hf_dataset(self):
+    def test(self):
         ds_train = create_hf_dataset.create_dataset(PROCESSED_TRAIN, LABELS, '', 'train')
         ds_test = create_hf_dataset.create_dataset(PROCESSED_TEST, LABELS, '', 'test')
         ds_dev = create_hf_dataset.create_dataset(PROCESSED_DEV, LABELS, '', 'validation')
@@ -37,7 +40,7 @@ class TestCreateHFDataset(unittest.TestCase):
         self.assertEqual(ds_dev.shape['validation'][0],121) # ccs + esd should have 121 sentences
 
 class TestTSDB_to_HF(unittest.TestCase):
-    def test_create_hf_dataset(self):
+    def test(self):
         tte = Token_Tag_Extractor()  # This extracts token-tag pairs, per corpus, sentences separated by special character
         lextypes = tte.parse_lexicons(LEXICONS)
         data = tte.process_testsuites(TREEBANKS, lextypes)
@@ -46,6 +49,27 @@ class TestTSDB_to_HF(unittest.TestCase):
         self.assertEqual(ds_train['train'].shape[0], 132) #mrs + pest should have 132 sentences
         #self.assertEqual(ds_test.shape['test'][0], 125) # psk + tgk should have 125 sentences
         #self.assertEqual(ds_dev.shape['validation'][0],121) # ccs + esd should have 121 sentences
+
+class TestTSDB_to_FeatVec(unittest.TestCase):
+    def test(self):
+        fve = Token_Tag_Extractor()  # This extracts token-tag pairs, per corpus, sentences separated by special character
+        lextypes = fve.parse_lexicons(LEXICONS)
+        data = fve.process_testsuites(TREEBANKS, lextypes)
+        self.assertEqual(len(data['train'][0].full_sentence_list),107) # should contain 107 sentences
+        self.assertEqual(len(data['train'][0].parsed_sentence_list), 107) # shoud parse all sentences
+        self.assertEqual(len(data['train'][0].processed_data), 107)  # should contain 107 token-tag pairs
+
+class TestTSDB_to_SCIKIT(unittest.TestCase):
+    def test(self):
+        fve = Token_Tag_Extractor()  # This extracts token-tag pairs, per corpus, sentences separated by special character
+        lextypes = fve.parse_lexicons(LEXICONS)
+        data = fve.process_testsuites(TREEBANKS, lextypes)
+        fve.write_output_by_split(TEST_DEST, data)
+        #ds_train = create_hf_dataset.create_dataset(TEST_DEST + '/train/train', LABELS, '', 'train')
+        #self.assertEqual(ds_train['train'].shape[0], 132) #mrs + pest should have 132 sentences
+        #self.assertEqual(ds_test.shape['test'][0], 125) # psk + tgk should have 125 sentences
+        #self.assertEqual(ds_dev.shape['validation'][0],121) # ccs + esd should have 121 sentences
+
 
 if __name__ == '__main__':
     unittest.main()
