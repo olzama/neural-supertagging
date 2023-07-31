b28b0ff0dff1e743ea29cebe79e582fa3101149b
olzama <olga.zamaraeva@gmail.com>
2023-07-31 15:22:09 +0200

    WIP mapping tokens to char spans

--- huggingface/tag_sentences.py
+++ huggingface/tag_sentences.py
@@ -7,6 +7,8 @@ import numpy as np
 from torch import nn, max as torch_max
 from train_on_dataset import compute_metrics
 import torch
+from delphin import itsdb
+from delphin.tokens import YYTokenLattice
 
 
 SPECIAL_TOKEN = -100
@@ -30,17 +32,40 @@ def test_eval_on_sentence(best_model, input_text, tokenizer):
     predicted_labels = [best_model.config.id2label[idx] for idx in max_prob_indices]
     print(predicted_labels)
 
+def extract_sentences(tsuite):
+    sentences = []
+    ts = itsdb.TestSuite(tsuite)
+    items = list(ts.processed_items())
+    for item in items:
+        words = []
+        terminals = None
+        lattice = None
+        char_spans = []
+        if len(item['results']) > 0:
+            terminals = item.result(0).derivation().terminals()
+        if (item['p-input']):
+            lattice = YYTokenLattice.from_string(item['p-input'])
+        if lattice and terminals:
+            for i, t in enumerate(terminals):
+                words.append(t.form)
+                char_spans.append({'terminal-form': t.form, 'token-form': lattice.tokens[i].form, 'span':lattice.tokens[i].lnk.data})
+        sentences.append({'sentence': item['i-input'], 'words':words, 'char_spans': char_spans})
+    return sentences
+
 
 def predict_tags_for_sentence(example, tokenizer, model, device):
-    tokenized_input_with_mapping = tokenizer(example, is_split_into_words=False, return_tensors="pt",
+    tokenized_input_with_mapping = tokenizer(example['sentence'], is_split_into_words=False, return_tensors="pt",
                                              return_offsets_mapping=True)
-    tokenized_input = tokenizer(example, is_split_into_words=False, return_tensors="pt")
+    pretokenized_input_with_mapping = tokenizer(example['words'], is_split_into_words=True, return_tensors="pt",
+                                             return_offsets_mapping=True)
+    tokenized_input = tokenizer(example['sentence'], is_split_into_words=False, return_tensors="pt")
     tokenized_input = {k: v.to(device) for k, v in tokenized_input.items()}
     tokens = tokenizer.convert_ids_to_tokens(tokenized_input["input_ids"][0])
     output = model(**tokenized_input)
     # Get character span for each token
     offset_mapping = tokenized_input_with_mapping['offset_mapping'][0].tolist()
     char_spans = [(start, end) for start, end in offset_mapping]
+    word_ids = pretokenized_input_with_mapping.word_ids()
     # Get the predicted labels for each token
     predicted_labels = torch.argmax(output.logits, dim=2)[0].tolist()
     char_spans_to_tags = {char_span: tag for char_span, tag in zip(char_spans, predicted_labels)}
@@ -54,9 +79,14 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = best_model.to(device)
-    example = 'The seeds produced as much as 20% corn.'
-    char_spans_to_tags = predict_tags_for_sentence(example, tokenizer, model, device)
+    sentences = extract_sentences(dataset_path)
+    #example = 'The seeds produced as much as 20% corn.'
+    all_predictions = []
+    for example in sentences:
+        char_spans_to_tags = predict_tags_for_sentence(example, tokenizer, model, device)
+        all_predictions.append(char_spans_to_tags)
     with open(output_path + '/predictions.txt', 'w') as f:
+        f.write(str(all_predictions))
         #f.write(json.dumps(char_spans_to_tags))
-        for char_span, tag in char_spans_to_tags.items():
-            f.write(str(char_span) + '\t' + str(tag) + '\n')
+        # for char_span, tag in char_spans_to_tags.items():
+        #     f.write(str(char_span) + '\t' + str(tag) + '\n')
